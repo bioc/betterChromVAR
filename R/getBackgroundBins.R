cf143dd724f774505ed2d4ee6b48b1efb0f53f2e
plger <pl.germain@gmail.com>
2026-03-31 11:12:04 +0200

    fixed wrong dimension!

--- R/getBackgroundBins.R
+++ R/getBackgroundBins.R
@@ -2,10 +2,13 @@
 #' 
 #' Get chromVAR-like bin-bin background selection probabilities
 #' 
-#' @param x A chromVAR counts object
-#' @param bias Vector of GCBias (default from object)
+#' @param x A SummarizedExperiment containing a 'counts' assay, or a matrix of
+#'   counts.
+#' @param bias Vector of GC bias (by default obtained from the object).
+#' @param flbias Vector of fragment length bias (by default obtained from the 
+#'   object). Not currently supported.
 #' @param w Standard deviation of the Gaussian kernel)
-#' @param bs Number of bins per dimension (total bins = bs^2)
+#' @param bs Number of bins per dimension.
 #' @param pseudo Optional pseudocount to be added. This should not be needed 
 #'   with standard workflows.
 #' 
@@ -25,38 +28,65 @@
 #' @examples
 #' counts_se <- getDummyData()$counts
 #' background <- getBackgroundBins(counts_se)
-getBackgroundBins <- function(x, bias=NULL, w=0.1, bs=50, pseudo=0){
+getBackgroundBins <- function(x, bias=NULL, flbias=NULL, w=0.1, bs=NULL,
+                              pseudo=0){
   if (inherits(x, "SummarizedExperiment") || 
       inherits(x, "SingleCellExperiment")) {
     if(is.null(bias)) bias <- rowData(x)$bias
-    x <- rowSums(assay(x))
+    if(is.null(flbias)) flbias <- rowData(x)$flbias
+    x <- rowSums(assay(x, "counts"))
   }else if(is.null(bias)){
     stopifnot("`bias` not provided, and not found in the object.")
   }
+  flbias <- NULL # disable; not currently implemented
+  stopifnot(length(bias)==nrow(x))
+  
+  if(!is.null(flbias)){
+    if(is.null(bs)) bs <- c(30L, 30L, 6L)
+    stopifnot(length(bs)==3)
+    stopifnot(length(flbias)==length(bias))
+  }else{
+    if(length(bs)==1) bs <- c(bs,bs)
+    stopifnot(length(bs)==2)
+  }
+  bs <- as.integer(bs)
+  stopifnot(all(bs>=1))
   
   # Mahalanobis transformation
   norm_mat <- cbind(log10(x+pseudo), bias)
+  if(!is.null(flbias)) norm_mat <- cbind(norm_mat, flbias)
   cv <- cov(norm_mat)
   diag(cv) <- diag(cv) + pseudo/1000
   transMat <- t(forwardsolve(t(chol(cv)), t(norm_mat)))
   
   # Calculate min/max for the bin boundaries
-  minCoords <- rowMins(transMat)
-  maxCoords <- rowMaxs(transMat)
+  minCoords <- colMins(transMat)
+  maxCoords <- colMaxs(transMat)
   range_coords <- maxCoords - minCoords
   
   # Map peak coordinates to bins
   idx1 <- round((transMat[, 1] - minCoords[1]) / 
-                  range_coords[1] * (bs - 1)) + 1
+                  range_coords[1] * (bs[1] - 1)) + 1
   idx2 <- round((transMat[, 2] - minCoords[2]) /
-                  range_coords[2] * (bs - 1)) + 1
+                  range_coords[2] * (bs[2] - 1)) + 1
   # Ensure indices fall within [1, bs]
-  idx1 <- pmax(1, pmin(bs, idx1))
-  idx2 <- pmax(1, pmin(bs, idx2))
+  idx1 <- pmax(1, pmin(bs[1], idx1))
+  idx2 <- pmax(1, pmin(bs[2], idx2))
+  
+  if(!is.null(flbias)){
+    idx3 <- round((transMat[, 3] - minCoords[3]) /
+                    range_coords[2] * (bs[3] - 1)) + 1
+    idx3 <- pmax(1, pmin(bs[3], idx3))
+  }
   
   # Linearize index
-  peak2bin <- idx1 + (idx2 - 1) * bs
-  binDensity <- tabulate(peak2bin, nbins = bs^2)
+  
+  peak2bin <- idx1 + (idx[, 2] - 1) * bs[1]
+  if(!is.null(flbias)){
+    peak2bin <- peak2bin  + (idx[, 3] - 1) * (bs[1] * bs[2])
+  }
+  
+  binDensity <- tabulate(peak2bin, nbins = prod(bs))
   
   # bin center grid (for distance calculation)
   bins1 <- seq(minCoords[1], maxCoords[1], length.out = bs)
